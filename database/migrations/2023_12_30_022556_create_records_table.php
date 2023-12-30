68534920d8ffe2d495a30be1e1209e36cfd322b4
MD TANVIR HASAN ANIK <md.tanvirhasananik@gmail.com>
2023-12-30 20:55:44 +0600

    add and edit api

---
+++ database/migrations/2023_12_30_022556_create_records_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('sector_id');
+            $table->foreign('sector_id')
+            ->references('id')
+            ->on('sectors')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+            $table->boolean('is_agreed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('records');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Sector;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Record;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +21,56 @@ Route::get('/sectors/all', function (Request $request) {
     $parentId = null; // Assuming the top-level categories have a null parent_id
     $hierarchy = Sector::getHierarchy($parentId);
     return $hierarchy;
+});
+
+
+Route::post('/record/add', function (Request $request) {
+    $rules = array(
+        'name' => 'required',
+        'selection' => 'required',
+        'agree' => 'required'
+    );
+
+    $validator = Validator::make($request->all(), $rules);
+
+    if ($validator->fails()) {
+        return response()->json('Invalid Data', 421);
+    }
+
+    $length = count($request->selection);
+
+    $record = Record::create([
+        'name' => $request->name,
+        'sector_id' => $request->selection[$length],
+        'is_agreed' => $request->agree
+    ]);
+    $res = $request->all();
+    $res['id'] = $record->id;
+    return $res;
+});
+
+
+Route::post('/record/update', function (Request $request) {
+    $rules = array(
+        'id' => 'required',
+        'name' => 'required',
+        'selection' => 'required',
+        'agree' => 'required'
+    );
+
+    $validator = Validator::make($request->all(), $rules);
+
+    if ($validator->fails()) {
+        return response()->json('Invalid Data', 421);
+    }
+
+    $length = count($request->selection);
+    $record =  Record::find($request->id);
+    $record->update([
+        'name' => $request->name,
+        'sector_id' => $request->selection[$length],
+        'is_agreed' => $request->agree
+    ]);
+    $res = $request->all();
+    return $res;
 });
